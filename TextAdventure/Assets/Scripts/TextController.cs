c081d1ba72e667f06f8494980bac2990b3590b35
Drithin <drithn@gmail.com>
2016-10-15 12:13:52 -0700

    Added UI elements of Text and Image and arranged it on a 2D screen, programmed the text by adding a C# script to the text component

---
+++ TextAdventure/Assets/Scripts/TextController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI; //we added UI element of text in the scene, we 'import' it into
+// code to program the UI text on the scene
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	public Text text; // Blue 'Text' element is coz of import UI function
+	// Use this for initialization
+	void Start () {
+		text.text = "Hello World";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+				text.text = "Space Key Pressed";
+		}
+	}
+}
