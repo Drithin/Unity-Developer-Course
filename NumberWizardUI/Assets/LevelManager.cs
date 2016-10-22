17f34b1ec4f06af503e0dd8369d24e0fb0e9ecb2
Drithin <drithn@gmail.com>
2016-10-22 08:29:43 -0700

    Added Start, Quit and Play Again buttons and configured button scripts on them. Arranged scenes in the Build settings. Learned how to call or move from one scene to other with a button click, say, start to play again and play again to start

---
+++ NumberWizardUI/Assets/LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// Methods are void coz we are not returning anything from them
+
+public class LevelManager : MonoBehaviour {
+	// functions doesn't show up in OnClick() tab
+	// we have to make method public for that
+	public void LoadLevel(string name){
+		Debug.Log (" Level loading requested for " + name);
+
+		Application.LoadLevel (name);
+
+
+	}
+
+	//Here we are not prompting to load or quit from a particular level
+	//so no point of a passing a variable to method
+	//We are quitting from the game itself!
+	public void QuitLevel(){
+		Debug.Log (" I want to Quit ");
+		Application.Quit ();
+
+
+	}
+}
