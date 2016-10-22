8899f643a6fd4e2a9542011a12974ed3917e5001
Drithin <drithn@gmail.com>
2016-10-22 11:32:23 -0700

    Linked all 4 scenes: Start, Game, Lose, Win with each other. Imported Number Wizard game code (drag and drop) and modified it according to UI game. Linked game objectto code to the UI elements like start, quit and restart. Used Random.Range() function to make the computer make a random guess rather than a binary tree. Limited number of guesses to the range 3-10

---
+++ NumberWizardUI/Assets/NumberWizrd.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizrd : MonoBehaviour {
+
+	int max; //wide scope
+	int min; // available to instance
+	int guess; 
+	public int maxGuessesAllowed=3;
+
+	public Text foo;
+
+
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+
+	void StartGame(){
+
+		print ("===============================================");
+
+		max = 1000; //wide scope
+		min = 1; // available to instance
+		GuessNext(); //need not say "int max" after declaring above
+		//it re-declares vars and create new set of vars of same names
+
+
+
+
+
+
+	}
+
+	public void GuessHigher(){
+		min = guess; //If guess is above 500, we move min to that level
+		GuessNext();
+	}
+
+	public void GuessLower (){
+		max = guess; //If guess is below 500, we move max to that level
+		GuessNext();
+	}
+
+	void GuessNext(){
+		guess = Random.Range(min, max+1);
+		foo.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel ("Win");
+		}
+
+	}
+}
