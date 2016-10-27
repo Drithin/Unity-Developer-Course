aaf48bda9385c0a0af4ef5194eb43413550a1dbb
Drithin <drithn@gmail.com>
2016-10-26 21:08:33 -0700

    Linked the created scenes with button scripts and OnClick component. Downloaded music from freesounds.org and added it to the game. Created Empty Game Object, named it music Player, added the downloaded track to it, drag and drop on game object precisely. Added C# script to it and called DontDestroyOnLoad function on Game Object

---
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// here gameObject mentioned in brackets refer to
+		// Music Player Game Object created in the Start Scene
+		GameObject.DontDestroyOnLoad (gameObject);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
