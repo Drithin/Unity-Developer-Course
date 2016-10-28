8ec44d60e85dc17966e52a981adef44810f96fa6
Drithin <drithn@gmail.com>
2016-10-27 21:11:33 -0700

    Learned Static Variables and Singletons. Also execution order of scripts. Music Player instance is coded in such a way that if present, DontDestroyLoadObject(game object) called on instance. In execution order, Awake, Start, Update are methods commonly called on every instance, 1st created instance's Awake() method is called first then the Awake() method of 2nd instance. Refer to order diagram in the end or refer to unity docs for execution order of functions

--- BlockBreaker/Assets/Scripts/MusicPlayer.cs
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -3,13 +3,39 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		// here gameObject mentioned in brackets refer to
-		// Music Player Game Object created in the Start Scene
-		GameObject.DontDestroyOnLoad (gameObject);
-	
+	static  MusicPlayer instance = null; // initiated a static variable instance to null
+
+	void Awake(){
+		
+
+		if (instance != null) { // if there isnt a music player instance, then
+			Destroy (gameObject); // Destroy the game object
+			print ("Game object self destructing");
+		} else {
+			instance = this;
+			// here gameObject mentioned in brackets refer to
+			// Music Player Game Object created in the Start Scene
+			GameObject.DontDestroyOnLoad (gameObject);
+
+			// instance = this; runs on START of the game and 'DontDestroyOnLoad'
+			//as we run the game
+		}
+
 	}
+
+	// Use this for initialization
+	//void Start () {
+
+
+		// Here we moved if clause to Awake method, we check console to realize
+		// that instance doesn't reach Start() method and game object gets destructed
+
+	//We comment Start() method and moved the if logic to Awake method to avoid
+	//delay in music playing between Awake() and Start() method
+
+
+
+	//}
 	
 	// Update is called once per frame
 	void Update () {
